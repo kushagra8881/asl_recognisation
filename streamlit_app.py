8654fb0134112637cdb4d37b4bdf1c265ac28e5c
kushagra8881 <kushagraparashar900@gmail.com>
2024-07-06 20:11:44 +0530

    Resolved merge conflicts

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import time
 # Load the model
 model_1 = load_model("asl_detection_model.h5")
-
+model_1.compile()
 # ASL dictionary
 asl_dict = {
     0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
